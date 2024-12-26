6d17f529ff9ea56e1d95eb9d5e352e142136f141
Sam Weaver <sam@samweaver.com>
2024-12-26 13:32:15 -0500

    Drop-in use of gc-arena
    (much more work to do)

--- lib-erlangrt/src/emulator/heap/heap_incremental.rs
+++ lib-erlangrt/src/emulator/heap/heap_incremental.rs
@@ -16,6 +16,9 @@ use crate::{
 use colored::Colorize;
 use log::{debug, trace};
 use core::fmt;
+use gc_arena::{
+  lock::RefLock, Arena, Collect, Gc as ArenaGc, Mutation, Rootable
+};
 
 /// Default heap size for constants (literals) when loading a module.
 const DEFAULT_LIT_HEAP: usize = 8192;
@@ -23,6 +26,20 @@ const DEFAULT_LIT_HEAP: usize = 8192;
 /// Default heap size when spawning a process. (default: 300)
 const DEFAULT_PROC_HEAP: usize = 1024;
 const BINARY_HEAP_CAPACITY: usize = 65536; // 64k*8 = 512kb
+
+#[derive(Collect)]
+#[collect(no_drop)]
+struct HeapData {
+  data: Vec<Word>,
+  /// Heap top, begins at 0 and grows up towards the `stack_top`.
+  heap_top: usize,
+  /// Stack top, begins at the end of capacity and grows down.
+  stack_top: usize,
+  /// Marks end of the stack and also end of the heap.
+  capacity: usize,
+}
+
+type HeapDataRef<'gc> = ArenaGc<'gc, RefLock<HeapData>>;
 
 /// A heap structure which allocates incrementally forward.
 /// Stack grows backwards until they meet with the heap.
@@ -33,16 +50,10 @@ where
   GC: TGc,
 {
   gc: GC,
-  data: Vec<Word>,
-  /// Heap top, begins at 0 and grows up towards the `stack_top`.
-  heap_top: usize,
-  /// Stack top, begins at the end of capacity and grows down.
-  stack_top: usize,
-  /// Marks end of the stack and also end of the heap.
-  capacity: usize,
+  arena: Arena<Rootable![HeapDataRef<'_>]>,
 }
 
-impl<GC: TGc> fmt::Debug for IncrementalHeap<GC> {
+impl<'gc, GC: TGc> fmt::Debug for IncrementalHeap<GC> {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(
       f,
@@ -53,92 +64,121 @@ impl<GC: TGc> fmt::Debug for IncrementalHeap<GC> {
   }
 }
 
-impl<GC: TGc> THeap for IncrementalHeap<GC> {
+impl<'gc, GC: TGc> THeap for IncrementalHeap<GC> {
   fn alloc(&mut self, n: SizeWords, fill: AllocInit) -> RtResult<*mut Word> {
-    let pos = self.heap_top;
-    let n_words = n.words;
+    self.arena.mutate(|mc, heap| {
+      let n_words = n.words;
 
-    // Explicitly forbid expanding without a GC, fail if capacity is exceeded
-    // This situation has to be detected before we arrive here
-    if pos + n_words >= self.stack_top {
-      panic!("Heap is full requested={}", n);
-    }
+      {
+        let heap = heap.borrow();
+        let pos = heap.heap_top;
 
-    // Assume we can grow the data without reallocating
-    let new_chunk = unsafe { self.get_heap_start_ptr_mut().add(self.heap_top) };
-
-    if fill == AllocInit::Nil {
-      let raw_nil = Term::nil().raw();
-      unsafe {
-        for i in 0..n_words {
-          new_chunk.add(i).write(raw_nil)
+        // Explicitly forbid expanding without a GC, fail if capacity is exceeded
+        // This situation has to be detected before we arrive here
+        if pos + n_words >= heap.stack_top {
+          panic!("Heap is full requested={}", n);
         }
       }
-    }
 
-    self.heap_top += n_words;
+      // Assume we can grow the data without reallocating
+      let new_chunk = unsafe { get_heap_start_ptr_mut(mc, heap).add(heap.borrow().heap_top) };
 
-    Ok(new_chunk)
+      if fill == AllocInit::Nil {
+        let raw_nil = Term::nil().raw();
+        unsafe {
+          for i in 0..n_words {
+            new_chunk.add(i).write(raw_nil)
+          }
+        }
+      }
+
+      {
+        let mut heap = heap.borrow_mut(mc);
+        heap.heap_top += n_words;
+      }
+
+      Ok(new_chunk)
+    })
+
   }
 
   #[inline]
   fn garbage_collect(&mut self, roots: Box<dyn TRootIterator>) -> RtResult<()> {
-    let walker = HeapWalker::new(self.get_heap_start_ptr_mut(), self.get_heap_top_ptr_mut());
-    GC::garbage_collect(self, walker, roots)
+    // TODO: garbage collection is still occurring outside of arena-gc, this
+    // should defer to arena-gc for the actual collection.
+    self.arena.mutate(|mc, heap| {
+      let walker = HeapWalker::new(get_heap_start_ptr_mut(mc, heap), get_heap_top_ptr_mut_impl(mc, heap));
+      GC::garbage_collect(self, walker, roots)
+    })
   }
 
   fn get_y(&self, index: Word) -> RtResult<Term> {
-    if !self.stack_have_y(index) {
-      debug!(
-        "Stack value requested y{}, depth={}",
-        index,
-        self.stack_depth()
-      );
-      return Err(RtErr::StackIndexRange(index));
-    }
-    let pos = index + self.stack_top + 1;
-    let result = Term::from_raw(self.data[pos]);
-    debug_assert!(result.is_value(), "Should never get a #Nonvalue<> from y[]");
-    Ok(result)
+    self.arena.mutate(|_, heap| {
+      if !self.stack_have_y(index) {
+        debug!(
+          "Stack value requested y{}, depth={}",
+          index,
+          stack_depth_impl(heap)
+        );
+        return Err(RtErr::StackIndexRange(index));
+      }
+    
+      let heap = heap.borrow();
+      let pos = index + heap.stack_top + 1;
+      let result = Term::from_raw(heap.data[pos]);
+      debug_assert!(result.is_value(), "Should never get a #Nonvalue<> from y[]");
+      Ok(result)
+    })
   }
 
   #[inline]
   fn get_y_unchecked(&self, index: Word) -> Term {
-    let pos = index + self.stack_top + 1;
-    Term::from_raw(self.data[pos])
+    self.arena.mutate(|_, heap| {
+      let heap = heap.borrow();
+      let pos = index + heap.stack_top + 1;
+      Term::from_raw(heap.data[pos])
+    })
   }
 
   /// Set stack value (`index`th from stack top) to `val`.
   fn set_y(&mut self, index: Word, val: Term) -> RtResult<()> {
-    debug_assert!(val.is_value(), "Should never set y[] to a #Nonvalue<>");
-    if !self.stack_have_y(index) {
-      return Err(RtErr::StackIndexRange(index));
-    }
-    if cfg!(feature = "trace_stack_changes") {
-      trace!("{}{} = {}", "set y".green(), index, val);
-    }
-    self.data[index + self.stack_top + 1] = val.raw();
-    Ok(())
+    self.arena.mutate(|mc, heap| {
+      let mut heap = heap.borrow_mut(mc);
+      debug_assert!(val.is_value(), "Should never set y[] to a #Nonvalue<>");
+      if !self.stack_have_y(index) {
+        return Err(RtErr::StackIndexRange(index));
+      }
+      if cfg!(feature = "trace_stack_changes") {
+        trace!("{}{} = {}", "set y".green(), index, val);
+      }
+      let new_pos = index + heap.stack_top + 1;
+      heap.data[new_pos] = val.raw();
+      Ok(())
+    })
   }
 
   /// Take `cp` from stack top and deallocate `n+1` words of stack.
   fn stack_deallocate(&mut self, n: usize) -> Term {
-    assert!(
-      self.stack_top + n < self.capacity,
-      "Failed to dealloc {}+1 words (s_top {}, s_end {})",
-      n,
-      self.stack_top,
-      self.capacity
-    );
+    self.arena.mutate(|mc, heap| {
+      let mut heap = heap.borrow_mut(mc);
+      assert!(
+        heap.stack_top + n < heap.capacity,
+        "Failed to dealloc {}+1 words (s_top {}, s_end {})",
+        n,
+        heap.stack_top,
+        heap.capacity
+      );
 
-    let cp = Term::from_raw(self.data[self.stack_top]);
-    assert!(
-      cp.is_cp(),
-      "Dealloc expected a CP value on stack top, got {}",
+      let cp = Term::from_raw(heap.data[heap.stack_top]);
+      assert!(
+        cp.is_cp(),
+        "Dealloc expected a CP value on stack top, got {}",
+        cp
+      );
+      heap.stack_top += n + 1;
+        
       cp
-    );
-    self.stack_top += n + 1;
-    cp
+    })
   }
 
   //  /// Express the intent to allocate `size` words on the heap, which may either
@@ -157,38 +197,42 @@ impl<GC: TGc> THeap for IncrementalHeap<GC> {
 
   #[inline]
   fn heap_check_available(&self, need: SizeWords) -> bool {
-    self.heap_top + need.words <= self.stack_top
+    self.arena.mutate(|_, heap| heap_check_available_impl(heap, need))
   }
 
   #[inline]
   fn stack_check_available(&self, need: SizeWords) -> bool {
-    self.heap_top + need.words <= self.stack_top
+    self.arena.mutate(|_, heap| stack_check_available_impl(heap, need))
   }
 
   /// Allocate stack cells without checking. Call `stack_have(n)` beforehand.
   fn stack_alloc(&mut self, need: SizeWords, _extra: SizeWords, fill: AllocInit) {
-    if need.words == 0 {
-      return;
-    }
-    self.stack_top -= need.words;
+    self.arena.mutate(|mc, heap| {
+      {
+        let mut heap = heap.borrow_mut(mc);
+        if need.words == 0 {
+          return;
+        }
+        heap.stack_top -= need.words;
+      }
 
-    // Clear the new cells
-    let raw_nil = Term::nil().raw();
-    unsafe {
-      let p = self.get_heap_start_ptr_mut().add(self.stack_top);
+      // Clear the new cells
+      let raw_nil = Term::nil().raw();
+      unsafe {
+        let p = get_heap_start_ptr_mut(mc, heap).add(heap.borrow().stack_top);
 
-      if fill == AllocInit::Nil {
-        for y in 0..need.words {
-          p.add(y).write(raw_nil)
+        if fill == AllocInit::Nil {
+          for y in 0..need.words {
+            p.add(y).write(raw_nil)
+          }
         }
       }
-    }
+    });
   }
 
   fn stack_depth(&self) -> usize {
-    self.capacity - self.stack_top
+    self.arena.mutate(|_, heap| stack_depth_impl(heap))
   }
-
 
   /// Push a Term to stack without checking. Call `stack_have(1)` beforehand.
   #[inline]
@@ -196,58 +240,72 @@ impl<GC: TGc> THeap for IncrementalHeap<GC> {
     if cfg!(feature = "trace_stack_changes") {
       trace!("{} {}", "push (unchecked)".green(), val);
     }
-    self.stack_top -= 1;
-    self.data[self.stack_top] = val.raw();
+    self.arena.mutate(|mc, heap| {
+      let mut heap = heap.borrow_mut(mc);
+      heap.stack_top -= 1;
+      let pos = heap.stack_top;
+      heap.data[pos] = val.raw();
+    });
   }
 
   /// Sets the stack top.
   /// Arg: new_stack_top - offset from the heap end
   fn drop_stack_words(&mut self, n_drop: usize) {
-    debug!("drop_stack_words {n_drop}");
-    assert!(self.stack_top + n_drop < self.capacity);
-    self.stack_top += n_drop;
+    self.arena.mutate(|mc, heap| {
+      debug!("drop_stack_words {n_drop}");
+      let mut heap = heap.borrow_mut(mc);
+      assert!(heap.stack_top + n_drop < heap.capacity);
+      heap.stack_top += n_drop;
+    });
   }
 
   /// Go through stack values searching for a stored CP, skip if it does not
   /// point to a catch instruction.
   /// Returns: the location stored on stack
   unsafe fn unroll_stack_until_catch(&self) -> Option<NextCatchResult> {
-    let mut ptr: *const Word = self.get_stack_top_ptr();
-    let stack_start: *const Word = self.get_stack_start_ptr();
-    // Counter how many stack cells to drop
-    let mut stack_drop = 0usize;
+    self.arena.mutate(|_, heap| {
 
-    loop {
-      if ptr >= stack_start {
-        return None;
-      }
-      // Hope we found a CP on stack (good!)
-      let term_at_ptr = Term::from_raw(ptr.read());
+      let mut ptr: *const Word = get_stack_top_ptr(heap);
+      let stack_start: *const Word = get_stack_start_ptr(heap);
+      // Counter how many stack cells to drop
+      let mut stack_drop = 0usize;
 
-      if term_at_ptr.is_catch() {
-        // Typical stack frame looks like:
-        // >>-top-> CP Catch ...
-        // Drop 1 less word than where the catch was found to preserve that CP
-        return Some(NextCatchResult {
-          loc: term_at_ptr.get_catch_ptr(),
-          stack_drop: stack_drop - 1,
-        });
+      loop {
+        if ptr >= stack_start {
+          return None;
+        }
+        // Hope we found a CP on stack (good!)
+        let term_at_ptr = Term::from_raw(ptr.read());
+
+        if term_at_ptr.is_catch() {
+          // Typical stack frame looks like:
+          // >>-top-> CP Catch ...
+          // Drop 1 less word than where the catch was found to preserve that CP
+          return Some(NextCatchResult {
+            loc: term_at_ptr.get_catch_ptr(),
+            stack_drop: stack_drop - 1,
+          });
+        }
+        ptr = ptr.add(1);
+        stack_drop += 1;
       }
-      ptr = ptr.add(1);
-      stack_drop += 1;
-    }
+    })
   }
 
   /// Create a constant iterator for walking the heap.
   /// This is used by heap walkers such as "dump.rs"
   unsafe fn heap_iter(&self) -> iter::HeapIterator {
-    let last = self.heap_top as isize;
-    let begin = self.get_heap_start_ptr() as *const Term;
-    iter::HeapIterator::new(begin, begin.offset(last))
+    self.arena.mutate(|_, heap| {
+      let begin = get_heap_start_ptr(heap) as *const Term;
+      let last = heap.borrow().heap_top as isize;
+      iter::HeapIterator::new(begin, begin.offset(last))
+    })
   }
 
   fn belongs_to_heap(&self, p: *const Word) -> bool {
-    p < self.get_heap_start_ptr() || p >= self.get_heap_top_ptr()
+    self.arena.mutate(|_, heap| {
+      p < get_heap_start_ptr(heap) || p >= get_heap_top_ptr_impl(heap)
+  })
   }
 
   fn stack_dump(&self) {
@@ -270,7 +328,7 @@ impl<GC: TGc> THeap for IncrementalHeap<GC> {
 
 // === === ===
 
-impl<GC: TGc> IncrementalHeap<GC> {
+impl<'gc, GC: TGc> IncrementalHeap<GC> {
   fn get_size_for(d: Designation) -> usize {
     match d {
       Designation::ProcessHeap => DEFAULT_PROC_HEAP,
@@ -284,83 +342,135 @@ impl<GC: TGc> IncrementalHeap<GC> {
   pub fn new(designation: Designation) -> Self {
     let capacity = Self::get_size_for(designation);
     assert!(capacity > 0);
-    let mut h = Self {
+
+    let arena = Arena::<Rootable![HeapDataRef<'_>]>::new(|mc| {
+      ArenaGc::new(mc, RefLock::new(HeapData {
+        data: Vec::with_capacity(capacity),
+        heap_top: 0,
+        stack_top: capacity,
+        capacity,
+      }))
+    });
+
+    let h = Self {
       gc: GC::new(),
-      data: Vec::with_capacity(capacity),
-      heap_top: 0,
-      stack_top: capacity,
-      capacity,
+      arena
     };
-    unsafe { h.data.set_len(capacity) };
+    
+    h.arena.mutate(|mc, heap| {
+      // this is safe since the data vector has not been touched yet
+      unsafe { heap.borrow_mut(mc).data.set_len(capacity) }
+    });
+
     h
   }
 
   /// How many words do we have before it will require GC/growth.
   #[inline]
   fn get_heap_max_capacity(&self) -> usize {
-    self.data.capacity()
+    // "Mutate" here refers to anything that is not GC, so we're not actually
+    // changing the data here.
+    self.arena.mutate(|_, heap| heap.borrow().data.capacity())
   }
 
   /// Heap usage stat.
   #[inline]
   fn get_heap_used_words(&self) -> usize {
-    self.heap_top
+    self.arena.mutate(|_, heap| heap.borrow().heap_top)
   }
 
   #[allow(dead_code)]
   #[inline]
   fn get_heap_available(&self) -> usize {
-    debug_assert!(self.stack_top > self.heap_top);
-    self.stack_top - self.heap_top
-  }
-
-  #[inline]
-  pub fn get_heap_start_ptr(&self) -> *const Word {
-    self.data.as_ptr()
-  }
-
-  #[inline]
-  fn get_heap_start_ptr_mut(&mut self) -> *mut Word {
-    self.data.as_mut_ptr()
+    self.arena.mutate(|_, heap| {
+      let heap = heap.borrow();
+      debug_assert!(heap.stack_top > heap.heap_top);
+      heap.stack_top - heap.heap_top
+    })
   }
 
   /// Get pointer to end of the allocated heap (below the stack top).
   #[inline]
   pub fn get_heap_top_ptr(&self) -> *const Word {
-    unsafe { self.get_heap_start_ptr().add(self.heap_top) }
+    self.arena.mutate(|_, heap| get_heap_top_ptr_impl(heap))
   }
 
   /// Get pointer to end of the allocated heap (below the stack top).
   #[inline]
   pub fn get_heap_top_ptr_mut(&mut self) -> *mut Word {
-    unsafe { self.get_heap_start_ptr_mut().add(self.heap_top) }
-  }
-
-  /// Stack start is same as end of everything, pointer to the first word after
-  /// the allocated memory, used as limit when iterating the stack.
-  #[inline]
-  unsafe fn get_stack_start_ptr(&self) -> *const Word {
-    self.get_end_ptr()
-  }
-
-  #[inline]
-  unsafe fn get_end_ptr(&self) -> *const Word {
-    self.get_heap_start_ptr().add(self.capacity)
-  }
-
-  #[inline]
-  unsafe fn get_stack_top_ptr(&self) -> *const Word {
-    self.get_heap_start_ptr().add(self.stack_top)
+    self.arena.mutate(|mc, heap| get_heap_top_ptr_mut_impl(mc, heap))
   }
 
   #[allow(dead_code)]
   pub fn stack_info(&self) {
-    debug!("Stack (s_top {}, s_end {})", self.stack_top, self.capacity)
+    self.arena.mutate(|_, data| {
+      let data = data.borrow();
+      debug!("Stack (s_top {}, s_end {})", data.stack_top, data.capacity)
+    });
   }
 
   /// Check whether `y+1`-th element can be found in stack
   #[inline]
   pub fn stack_have_y(&self, y: Word) -> bool {
-    self.capacity - self.stack_top >= y + 1
+    self.arena.mutate(|_, data| {
+      let data = data.borrow();
+      data.capacity - data.stack_top >= y + 1
+    })
   }
+}
+
+fn stack_depth_impl<'gc>(heap: &HeapDataRef<'gc>) -> usize {
+  let heap = heap.borrow();
+  heap.capacity - heap.stack_top
+}
+
+#[inline]
+fn heap_check_available_impl<'gc>(heap: &HeapDataRef<'gc>, need: SizeWords) -> bool {
+  let heap = heap.borrow();
+  heap.heap_top + need.words <= heap.stack_top
+}
+
+#[inline]
+fn stack_check_available_impl<'gc>(heap: &HeapDataRef<'gc>, need: SizeWords) -> bool {
+  let heap = heap.borrow();
+  heap.heap_top + need.words <= heap.stack_top
+}
+  
+fn get_heap_top_ptr_impl<'gc>(heap: &HeapDataRef<'gc>) -> *const Word {
+  let ptr = get_heap_start_ptr(heap);
+  unsafe { ptr.add(heap.borrow().heap_top) }
+}
+
+#[inline]
+pub fn get_heap_start_ptr<'gc>(heap: &HeapDataRef<'gc>) -> *const Word {
+  heap.borrow().data.as_ptr()
+}
+
+#[inline]
+fn get_heap_start_ptr_mut<'gc>(mc: &Mutation<'gc>, heap: &HeapDataRef<'gc>) -> *mut Word {
+  heap.borrow_mut(mc).data.as_mut_ptr()
+}
+
+fn get_heap_top_ptr_mut_impl<'gc>(mc: &Mutation<'gc>, heap: &HeapDataRef<'gc>) -> *mut Word {
+  let ptr = get_heap_start_ptr_mut(mc, heap);
+  unsafe { ptr.add(heap.borrow().heap_top) }
+}
+
+/// Stack start is same as end of everything, pointer to the first word after
+/// the allocated memory, used as limit when iterating the stack.
+#[inline]
+unsafe fn get_stack_start_ptr<'gc>(heap: &HeapDataRef<'gc>) -> *const Word {
+  get_end_ptr(heap)
+}
+
+#[inline]
+unsafe fn get_end_ptr<'gc>(heap: &HeapDataRef<'gc>) -> *const Word {
+  let ptr = get_heap_start_ptr(heap);
+  ptr.add(heap.borrow().capacity)
+}
+
+#[inline]
+unsafe fn get_stack_top_ptr<'gc>(heap: &HeapDataRef<'gc>) -> *const Word {
+  let ptr = get_heap_start_ptr(heap);
+  ptr.add(heap.borrow().stack_top)
 }
